f93c06d73b5b5eb91777e30e64cb5d6392743b05
affandeZone <affandes@gmail.com>
2016-02-19 14:52:42 +0700

    2016-02-19 14:36 Update file TestingTeamCity.java

---
+++ src/m1/Persegi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package m1;
+
+/**
+ *
+ * @author affandeZone
+ */
+public class Persegi {
+    
+    // Atribut
+    public double panjang;
+    public double lebar;
+    public double luas;
+    
+    // Konstruktor
+    public Persegi(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.luas = panjang * lebar /0;
+    }
+    
+}
